08266d8a759549c1033b23db26a171eab36eb5f4
ScriptKiddie404 <fernando.puentes.developer@gmail.com>
2021-12-08 18:02:28 -0700

    Layout principal

---
+++ src/app/Components/Counter/counter.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'contador',
+  templateUrl: './counter.component.html',
+  styleUrls: ['./counter.component.css'],
+})
+export class CounterComponent {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './Components/Header/header.component';
+import { CounterComponent } from './Components/Counter/counter.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HeaderComponent, CounterComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
